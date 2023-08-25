3fa7dae872884c384fd88a4daa116acd8822bd97
Eduardo Macedo <eduzemacedo@hotmail.com>
2023-08-25 17:39:44 -0300

    feat: logs exceptions handled by exception handler filter

--- src/Natter.Api/Filters/ExceptionHandlerFilter.cs
+++ src/Natter.Api/Filters/ExceptionHandlerFilter.cs
@@ -5,9 +5,14 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 public class ExceptionHandlerFilter : IExceptionFilter
 {
+    private readonly ILogger<ExceptionHandlerFilter> _logger;
+
+    public ExceptionHandlerFilter(ILogger<ExceptionHandlerFilter> logger) => _logger = logger;
+
     public void OnException(ExceptionContext context)
     {
         var excp = context.Exception;
+        _logger.LogError(excp, "Some error ocurred while processing the request.");
 
         context.Result = new JsonResult(new { ErrorMessage = excp.Message })
         {
